82e0ce53c0f867b031c4f97f6e3133f90cb21155
sean1 <G00402034@atu.ie>
2024-09-23 08:19:01 +0100

    added methods for hello and name

---
+++ src/main/java/ie/atu/week2_refresher/Week2Refresher.java
@@ -0,0 +1,20 @@
+package ie.atu.week2_refresher;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Week2Refresher {
+
+    @GetMapping("/hello")
+            public String Message()
+    {
+        return "Hello";
+    }
+    @GetMapping("/hello/{name}")
+    public String myName(@PathVariable String name)
+    {
+        return "Your name is " + name;
+    }
+}
