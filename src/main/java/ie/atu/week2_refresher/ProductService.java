c346364be5d3c02980984b1acfb23982d9ddd674
G00402034@atu.ie <G00402034@atu.ie>
2024-09-23 13:26:30 +0100

    finished First part of lab

--- src/main/java/ie/atu/week2_refresher/ProductService.java
+++ src/main/java/ie/atu/week2_refresher/ProductService.java
@@ -1,5 +1,31 @@
 package ie.atu.week2_refresher;
 
-public class ProductService {
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+    public class ProductService {
+        private List<Product> productList = new ArrayList<>();
+        public List<Product> getAllProducts() {
+            return productList;
+        }
+    @PostMapping
+        public Product addProduct(@RequestBody Product product) {
+            productList.add(product);
+            return product;
+        }
+
+
+
+
 
 }
+
+    }
+
+
