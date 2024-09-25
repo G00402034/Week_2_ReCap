853541c950f2ec36237a04aa9efd13f7eac60849
sean1 <G00402034@atu.ie>
2024-09-25 18:51:03 +0100

    added methods for getting all products and adding products in product service

--- src/main/java/ie/atu/week2_refresher/Product_Service.java
+++ src/main/java/ie/atu/week2_refresher/Product_Service.java
@@ -1,4 +1,20 @@
 package ie.atu.week2_refresher;
 
-public class Product_Service {
-}
+
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+    @Service
+    public class Product_Service {
+        private List<Product> productlist = new ArrayList<Product>();
+
+        public List<Product> getAllProducts() {
+            return productlist;
+        }
+        public Product addProduct(Product product) {
+            productlist.add(product);
+            return product;
+        }
+    }
+
