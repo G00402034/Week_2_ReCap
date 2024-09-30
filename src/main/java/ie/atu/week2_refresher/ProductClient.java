af9145fa8b3a2ba6f72a1797cbf71c221328775b
G00402034 <G00402034@atu.ie>
2024-09-30 17:33:46 +0100

    added endpoint for interface

--- src/main/java/ie/atu/week2_refresher/ProductClient.java
+++ src/main/java/ie/atu/week2_refresher/ProductClient.java
@@ -2,8 +2,12 @@ package ie.atu.week2_refresher;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "ProductClient", url = "http:localhost:8081")
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "Product", url = "http:localhost:8081")
 public interface ProductClient {
 
-
+@PostMapping("/product")
+    String ProductDetails(@RequestBody Product product);
 }
